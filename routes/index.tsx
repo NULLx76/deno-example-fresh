e17276cbf57554b97665f33c337296487a9af9d6
Vivian <vivian@0x76.dev>
2025-11-17 13:22:19 +0100

    test dev

--- routes/index.tsx
+++ routes/index.tsx
@@ -20,8 +20,7 @@ export default define.page(function Home(ctx) {
         />
         <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
         <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
+          This pages is defined in: <code class="mx-2">./routes/index.tsx</code>.
         </p>
         <Counter count={count} />
       </div>
